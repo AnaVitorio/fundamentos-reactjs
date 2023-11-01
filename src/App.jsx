ada75c264a2003083660ed2363591ad8cc6136b9
Ana Carolina <anavitorio.p@gmail.com>
2023-11-01 20:08:10 -0300

    criando o Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,11 @@
 import { Post } from "./Post"
+import { Header } from "./components/Header";
+import './global.css';
 
 export function App() {
   return (
     <div>
+      <Header/>
       <Post 
         author="Ana" 
         content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eaque minus reiciendis molestias dignissimos cupiditate vero accusamus reprehenderit, repudiandae, cum maiores! Dolorum, nulla sapiente itaque et provident corrupti natus animi."
--- src/Post.jsx
+++ src/Post.jsx
@@ -1,3 +1,5 @@
+import { Header } from "./components/Header";
+
 //named export
 export function Post(props){
     return (
