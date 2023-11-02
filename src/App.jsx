1baddbf6d498d769f80df786dc2e72fdfb093872
Ana Carolina <anavitorio.p@gmail.com>
2023-11-02 16:48:11 -0300

    Criando Sidebar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
-import { Post } from "./Post"
+import { Post } from "./Post";
 import { Header } from "./components/Header";
-import './global.css';
+import "./global.css";
+import styles from "./App.module.css";
+import { Sidebar } from "./components/Sidebar";
 
 export function App() {
   return (
     <div>
-      <Header/>
-      <Post 
-        author="Ana" 
-        content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eaque minus reiciendis molestias dignissimos cupiditate vero accusamus reprehenderit, repudiandae, cum maiores! Dolorum, nulla sapiente itaque et provident corrupti natus animi."
-      />
-      <Post
-        author="Diego"
-        content="Conteúdo 2"
-      />
+      <Header />
+      <div className={styles.wrapper}>
+        <Sidebar/>
+        <main>
+          <Post
+            author="Ana"
+            content="Lorem ipsum dolor sit amet consectetur adipisicing elit. Cumque eaque minus reiciendis molestias dignissimos cupiditate vero accusamus reprehenderit, repudiandae, cum maiores! Dolorum, nulla sapiente itaque et provident corrupti natus animi."
+          />
+          <Post author="Diego" content="Conteúdo 2" />
+        </main>
+      </div>
     </div>
-  )
+  );
 }
-
